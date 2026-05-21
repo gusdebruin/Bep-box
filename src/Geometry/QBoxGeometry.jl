2ff737c4f2a61e870a2cf17f5647ad8505f2b8d5
Gus de Bruin <g.debruin-2@student.tudelft.nl>
2026-05-21 12:19:41 +0200

    feat: I added the functions get_qbox_element_ids, get_child_qbox_ids and refine_qbox! to QBoxGeometry, I changed them from QBoxInfo such that they work for QBoxGeometry.

--- src/Geometry/QBoxGeometry.jl
+++ src/Geometry/QBoxGeometry.jl
@@ -28,8 +28,8 @@ end
 
 function QBoxGeometry(geom::AbstractGeometry{manifold_dim, image_dim, num_patches}, 
     qbox_size::NTuple{manifold_dim,Int}) where {manifold_dim, image_dim, num_patches}
-    refined_geom = subdivide_geometry(geom, qbox_size)
-    n_elements = get_lin_num_elements(refined_geom)
+    refined_geom = subdivide_geometry(geom, qbox_size) #from which module is this??
+    n_elements = get_lin_num_elements(refined_geom) #from which module is this??
     active_elements = Hierarchy.ActiveInfo([collect(1:n_elements)])
     hier_geom = HierarchicalGeometry((refined_geom,), active_elements)
     return QBoxGeometry(hier_geom, qbox_size)
@@ -74,12 +74,13 @@ Returns the id of the pbox a element 'hier_id' is in. It uses these steps:
 - `patch_id::Int`: The patch ID
 """
 function get_qbox_id_hier(hier_id::Int, qbox_geometry::QBoxGeometry)
-    hg = get_hierarchical_geometry(qbox_geometry)
+    hier_geom = get_hierarchical_geometry(qbox_geometry)
     # hier_id →  convert_to_level_and_level_id (from ActiveInfo --> module Hierarchy)  →  level + level_id
-    level, level_id = Hierarchy.convert_to_level_and_level_id(get_active_elements(hg), hier_id)
+    level, level_id = Hierarchy.convert_to_level_and_level_id(get_active_elements(hier_geom), hier_id)
 
     # level_id →  get_patch_and_local_element_id (from module Geometry) →  patch_id + local_id
-    patch_id, local_element_id = get_patch_and_local_element_id(get_geometries(hg)[level], level_id)
+    geom_level = get_level_geometry(hier_geom, level)
+    patch_id, local_element_id = get_patch_and_local_element_id(geom_level, level_id)
 
     qbox_id= get_qbox_id_local(level, local_element_id, patch_id, qbox_geometry)
 
@@ -88,17 +89,17 @@ end
 
 function get_qbox_id_local(level::Int, local_element_id::Int, patch_id::Int, qbox_geometry::QBoxGeometry)
     # find qbox size and n_elements of the correct level
-    size_qbox = get_qbox_size(qbox_geometry)  # NTuple{manifold_dim,Int}
-    hg = get_hierarchical_geometry(qbox_geometry)
-    geom = get_geometries(hg)[level]
-    patch_geom = get_parametric_geometry(geom, patch_id)
+    size_qbox = get_qbox_size(qbox_geometry)
+    hier_geom = get_hierarchical_geometry(qbox_geometry)
+    geom_level = get_level_geometry(hier_geom, level)
+    patch_geom = get_parametric_geometry(geom_level, patch_id)
     n_elements_dim = get_cart_num_elements(patch_geom)
-    
+
     # qbox_size + n_elements + local_id → qbox_id
-    coords = CartesianIndices(n_elements_dim)[local_element_id]
+    coords = Points.CartesianIndices(n_elements_dim)[local_element_id]
     qbox_coords = ntuple(i -> (coords[i] - 1) ÷ size_qbox[i] + 1, length(size_qbox))
     n_qboxes_dim = ntuple(i -> n_elements_dim[i] ÷ size_qbox[i], length(size_qbox))
-    qbox_id = LinearIndices(n_qboxes_dim)[qbox_coords]
+    qbox_id = Points.LinearIndices(n_qboxes_dim)[qbox_coords]
     return qbox_id
 end
 
@@ -119,19 +120,24 @@ Returns the ids of the elements of a pbox 'pbox_id'.
 # Returns
 - `ids::Vector{Int}`: Returns a Vector{Int} with the level-local element IDs
 """
-function get_qbox_element_ids(qbox_id::Int, qbox_info::QBoxInfo, level::Int, patch_id::Int)
-    q = get_qbox_size(qbox_info)    
-    n_elements = get_n_elements_dim(qbox_info, level)
-    n_qboxes = get_n_qboxes_dim(qbox_info, level)
-    qbox_coords = CartesianIndices(n_qboxes)[qbox_id]
-
+function get_qbox_element_ids(level::Int, patch_id::Int, qbox_geometry::QBoxGeometry, qbox_id::Int)
+    size_qbox = get_qbox_size(qbox_geometry)
+    hier_geom = get_hierarchical_geometry(qbox_geometry)
+    geom_level = get_level_geometry(hier_geom, level)
+    patch_geom = get_parametric_geometry(geom_level, patch_id)
+    n_elements_dim = get_cart_num_elements(patch_geom)
+    n_qboxes_dim = ntuple(i -> n_elements_dim[i] ÷ size_qbox[i], length(size_qbox))
+    qbox_coords = Points.CartesianIndices(n_qboxes_dim)[qbox_id]
+    
+    # hier nog naar kijken (is misschien efficienter:)
+    # n = prod(size_qbox)
+    # ids = Vector{Int}(undef, n)
     ids = Int[]
-    element_ranges = ntuple(i -> (qbox_coords[i]-1)*q[i]+1 : qbox_coords[i]*q[i], length(q))
-    geom = get_qbox_geometry(qbox_info)
+    element_ranges = ntuple(i -> (qbox_coords[i]-1)*size_qbox[i]+1 : qbox_coords[i]*size_qbox[i], length(size_qbox))
     for element_coords in Iterators.product(element_ranges...)
-        element_local_id = LinearIndices(n_elements)[element_coords...]
-        element_level_id = get_global_element_id(geom, patch_id, element_local_id)
-        push!(ids, element_level_id)
+        local_element_id = Points.LinearIndices(n_elements_dim)[element_coords...]
+        level_element_id = get_global_element_id(geom_level, patch_id, local_element_id)
+        push!(ids, level_element_id)
     end
     return ids
 end
@@ -151,23 +157,41 @@ pboxes in which 'pbox_id' has been refined to in the next level.
 - `children::Vector{Int}`: Returns a Vector{Int} with IDs of the children
 
 """
-function child_qbox_ids(qbox_info::QBoxInfo, level::Int, qbox_id::Int)
-    refinement = get_refinement_factors(qbox_info, level)
-    n_qboxes_parent = get_n_qboxes_dim(qbox_info, level)
-    n_qboxes_child  = get_n_qboxes_dim(qbox_info, level + 1)
-    
-    qbox_coords = CartesianIndices(n_qboxes_parent)[qbox_id]
+function get_child_qbox_ids(level::Int, patch_id::Int, qbox_geometry::QBoxGeometry, qbox_id::Int)
+    size_qbox = get_qbox_size(qbox_geometry)
+    hier_geom = get_hierarchical_geometry(qbox_geometry)
 
-    offset_ranges = ntuple(i -> 0:(refinement[i]-1), length(refinement))
-    offsets = Iterators.product(offset_ranges...)
-
-    children = Int[]
-    for off in offsets
-        child_coords = ntuple(i -> (qbox_coords[i]-1)*refinement[i] + off[i] + 1, length(refinement))
-        child_id = LinearIndices(n_qboxes_child)[child_coords...]
-        push!(children, child_id)
+    # check if level isn't the last level
+    last_level = get_num_levels(hier_geom)
+    if level == last_level
+        throw(
+                ArgumentError(
+                    "Level of QBox is last level, create new geometry for new level"
+                ),
+            )
     end
 
+    geom_parent = get_level_geometry(hier_geom, level)
+    patch_geom_parent = get_parametric_geometry(geom_parent, patch_id)
+    n_elements_dim_parent = get_cart_num_elements(patch_geom_parent)
+
+    geom_child = get_level_geometry(hier_geom, level+1)
+    patch_geom_child = get_parametric_geometry(geom_child, patch_id)
+    n_elements_dim_child = get_cart_num_elements(patch_geom_child)
+
+    rf = ntuple(i -> n_elements_dim_child[i] ÷ n_elements_dim_parent[i], length(n_elements_dim_parent))
+
+    n_qboxes_dim_parent = ntuple(i -> n_elements_dim_parent[i] ÷ size_qbox[i], length(size_qbox))
+    n_qboxes_dim_child = ntuple(i -> n_elements_dim_child[i] ÷ size_qbox[i], length(size_qbox))
+
+    parent_coords = Points.CartesianIndices(n_qboxes_dim_parent)[qbox_id]
+    child_ranges = ntuple(i -> ((parent_coords[i]-1)*rf[i] + 1) : (parent_coords[i]*rf[i]), length(rf))
+
+    children = Int[]
+    for child_coords in Iterators.product(child_ranges...)
+        child_id = LinearIndices(n_qboxes_dim_child)[child_coords...]
+        push!(children, child_id)
+    end
     return children
 end
 
@@ -193,14 +217,16 @@ the children will be set to active, and the elements of the pbox no longer.
 - `pbox_id::Int`: The pbox ID of the pbox that we want to refine.
 
 """
-function refine_qbox!(qbox_info::QBoxInfo, level::Int, patch_id::Int, qbox_id::Int)
-    active = get_qbox_active_info(qbox_info)
-    remove = get_qbox_element_ids(qbox_id, qbox_info, level, patch_id)
-
-    children = child_qbox_ids(qbox_info, level, qbox_id)
+function refine_qbox!(qbox_geometry::QBoxGeometry, level::Int, patch_id::Int, qbox_id::Int)
+    hier_geom = get_hierarchical_geometry(qbox_geometry)
+    active = get_active_elements(hier_geom)
+    
+    remove = get_qbox_element_ids(level, patch_id, qbox_geometry, qbox_id)
+    
+    children = get_child_qbox_ids(level, patch_id, qbox_geometry, qbox_id)
     add = Int[]  
     for child in children
-        append!(add, get_qbox_element_ids(child, qbox_info, level+1, patch_id))
+        append!(add, get_qbox_element_ids(level+1, patch_id, qbox_geometry, child))
     end
     update!(active, level, remove, add)
 end
