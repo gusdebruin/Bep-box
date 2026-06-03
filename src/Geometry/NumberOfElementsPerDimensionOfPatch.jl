669c8252a2c55a4a6988e6aa39f6015e11be02a7
Gus de Bruin <g.debruin-2@student.tudelft.nl>
2026-06-03 17:02:09 +0200

    feat: I added the functionality for a TensorProductGeometry, but I still need to test  it.

--- src/Geometry/NumberOfElementsPerDimensionOfPatch.jl
+++ src/Geometry/NumberOfElementsPerDimensionOfPatch.jl
@@ -33,6 +33,23 @@ function get_n_elements_patch_dim(geom::UnstructuredGeometry, patch_id::Int)
     return n_elements_dim
 end 
 
+function get_n_elements_patch_dim(
+    geom::TensorProductGeometry{manifold_dim, image_dim, num_patches}, 
+    patch_id::Int
+    ) where {manifold_dim, image_dim, num_patches}
+    constituent_geoms = get_constituent_geometries(geom)
+    constituent_num_patches = map(get_num_patches, constituent_geoms)
+    cart_num_patches = CartesianIndices(constituent_num_patches)
+    patch_tuple = cart_num_patches[patch_id]
+
+    dims_per_const = map(
+        (g, pid) -> get_n_elements_patch_dim(g, pid),
+        constituent_geoms,
+        Tuple(patch_tuple)
+    )
+    return Tuple(vcat(dims_per_const...))
+end 
+
 function get_n_elements_patch_dim(geom::HierarchicalGeometry, level::Int, patch_id::Int)
     level_geom = get_level_geometry(geom, level)
     return get_n_elements_patch_dim(level_geom, patch_id)
