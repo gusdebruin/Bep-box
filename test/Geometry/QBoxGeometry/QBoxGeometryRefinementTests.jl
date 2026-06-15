a5e3a5eefd16bfc7515cfff440634db2014a1cdb
Gus de Bruin <g.debruin-2@student.tudelft.nl>
2026-06-15 15:17:15 +0200

    tests: I added some tests for refinement, they all passed.

---
+++ test/Geometry/QBoxGeometry/QBoxGeometryRefinementTests.jl
@@ -0,0 +1,32 @@
+module QBoxGeometryRefinementTests
+
+using Mantis
+using Test
+
+############################################################################################
+#                                          Setup                                           #
+############################################################################################
+
+#2D QBox
+geom_2D = Geometry.CartesianGeometry((
+    0.0:2.0:5.0,
+    0.0:2.0:5.0
+))
+n1_2D = Geometry.get_num_elements(geom_2D)   # = 16
+qbox_size_2D = (2,2)
+num_subdivisions_2D = (2,1)
+qbg_2D = Geometry.QBoxGeometry_refine(geom_2D, qbox_size_2D, num_subdivisions_2D)
+#Plot.plot(qbg_2D; vtk_filename="Starting Geometry Refinement Tests")
+
+@testset "QBox Refinement tests 2D" begin
+    error = [0.05, 0.05, 0.4, 0.3, 0.05, 0.05, 0.3, 0.4, 0.05, 0.8, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05]
+    marked_qboxes = Geometry.refine_qboxgeom_max!(qbg_2D, error, 0.1 )
+    @test marked_qboxes == Set([(3,1,1)])
+    #Plot.plot(qbg_2D; vtk_filename="Refinement after max")
+    error2 = [0.05, 0.05, 0.4, 0.3, 0.05, 0.05, 0.3, 0.4,0.05, 0.05, 0.05, 0.05, 0.35, 0.33,0.34, 0.34, 0.05, 0.05, 0.05, 0.05]
+    marked_qboxes2 = Geometry.refine_qboxgeom_avg!(qbg_2D, error2, 0.1 )
+    @test marked_qboxes2 == [(5,2,1),(2,1,1)]
+    #Plot.plot(qbg_2D; vtk_filename="Refinement after avg")
+end
+
+end
